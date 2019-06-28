3aade204aa04d6069063e2f6c2216933a9394c08
Anwesh43 <anweshthecool0@gmail.com>
2019-06-28 11:21:11 +0530

    adding constants and importing libraries

--- squarebiexpandlineview/src/main/java/com/anwesh/uiprojects/squarebiexpandlineview/SquareBiExpandView.kt
+++ squarebiexpandlineview/src/main/java/com/anwesh/uiprojects/squarebiexpandlineview/SquareBiExpandView.kt
@@ -3,3 +3,21 @@ package com.anwesh.uiprojects.squarebiexpandlineview
 /**
  * Created by anweshmishra on 28/06/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.RectF
+import android.graphics.Color
+import android.app.Activity
+import android.content.Context
+
+val nodes : Int = 5
+val square : Int = 4
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#2196F3")
+val backColor : Int = Color.parseColor("#BDBDBD")
