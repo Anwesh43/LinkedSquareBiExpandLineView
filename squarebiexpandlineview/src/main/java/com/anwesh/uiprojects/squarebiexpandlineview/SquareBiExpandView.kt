dcd258879edf132a2fde1f0a2f987ee600bef74d
Anwesh43 <anweshthecool0@gmail.com>
2019-06-28 12:12:49 +0530

    adding SquareBiExpandLine class

--- squarebiexpandlineview/src/main/java/com/anwesh/uiprojects/squarebiexpandlineview/SquareBiExpandView.kt
+++ squarebiexpandlineview/src/main/java/com/anwesh/uiprojects/squarebiexpandlineview/SquareBiExpandView.kt
@@ -173,4 +173,28 @@ class SquareBiExpandLineView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class SquareBiExpandLine(var i : Int) {
+
+        private val root : SBENode = SBENode(0)
+        private var curr : SBENode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
