f1f371ba2485f0f4cf0b13c84449283a696a8c39
Anwesh43 <anweshthecool0@gmail.com>
2019-06-28 12:24:58 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedsquarebiexpandlineview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedsquarebiexpandlineview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedsquarebiexpandlineview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.squarebiexpandlineview.SquareBiExpandLineView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        SquareBiExpandLineView.create(this)
     }
 }
