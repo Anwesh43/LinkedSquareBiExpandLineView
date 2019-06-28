c6ff0af632fe96095ef0b321eb3ab9bb9a32ffb6
Anwesh43 <anweshthecool0@gmail.com>
2019-06-28 11:52:53 +0530

    adding SBENode class

--- squarebiexpandlineview/src/main/java/com/anwesh/uiprojects/squarebiexpandlineview/SquareBiExpandView.kt
+++ squarebiexpandlineview/src/main/java/com/anwesh/uiprojects/squarebiexpandlineview/SquareBiExpandView.kt
@@ -125,8 +125,52 @@ class SquareBiExpandLineView(ctx : Context) : View(ctx) {
 
         fun stop() {
             if (animated) {
-                animated = false 
+                animated = false
             }
+        }
+    }
+
+    data class SBENode(var i : Int, val state : State = State()) {
+
+        private var next : SBENode? = null
+        private var prev : SBENode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = SBENode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSBENode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SBENode {
+            var curr : SBENode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
         }
     }
 }
